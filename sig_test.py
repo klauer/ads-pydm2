e7e863647c2c49fa167906a77ec6c6e5becf30bf
K Lauer <klauer@slac.stanford.edu>
2019-07-19 14:49:36 -0700

    TMP: Device test

--- sig_test.py
+++ sig_test.py
@@ -1,3 +1,14 @@
+from ophyd import Device, Component as Cpt
+
 from ads_pcds import AdsSignal
 
-sig = AdsSignal('ads://172.21.148.145/Main.iCycle', name='sig')
+# sig = AdsSignal('ads://172.21.148.145/Main.iCycle', name='sig')
+sig = AdsSignal('ads://172.21.148.145/@1/Main.iCycle', name='sig')
+
+
+class MyDevice(Device):
+    icycle_polled = Cpt(AdsSignal, 'Main.iCycle', poll_rate=1.0)
+    brake = Cpt(AdsSignal, 'Main.M1.bBrake')
+
+
+dev = MyDevice('ads://172.21.148.145/', name='dev')
